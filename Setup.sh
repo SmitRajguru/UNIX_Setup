8cb1209e35e0f239269dd711ecfd844a524374e1
Smit Rajguru <rajgurusmit1997@gmail.com>
2023-11-09 18:19:49 -0500

    ros2 and commands seperated

--- Setup.sh
+++ Setup.sh
@@ -4,11 +4,11 @@
 if grep -qi microsoft /proc/version; then
   # echo "Ubuntu on Windows"
   isWSL=true
-  catkinRoot = "/mnt/c/"
+  catkinRoot="/mnt/c"
 else
   # echo "Native Linux"
   isWSL=false
-  catkinRoot = "~/"
+  catkinRoot="~"
 fi
 
 # Detect Linux version codename
@@ -17,15 +17,22 @@ if [ "$distro" = "focal" ]; then
   echo "Running on Ubuntu 20.04 - Focal Fossa"
   ROS_DISTRO="noetic"
 elif [ "$distro" = "jammy" ]; then
-  echo "Running on Ubuntu 21.10 - Jammy Jellyfish"
+  echo "Running on Ubuntu 22.04 - Jammy Jellyfish"
   ROS_DISTRO="humble"
 else
   echo "Unknown Linux version"
 fi
 
+echo "WSL: $isWSL"
+echo "Catkin Root: $catkinRoot"
+echo "ROS Distro: $ROS_DISTRO"
+
 # add to /etc/sudoers file
 echo "Adding user to sudoers for no password"
 echo $USER "ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers
+
+# copy bash_aliases.sh to ~/.bash_aliases
+sudo cp ./bash_aliases.sh  ~/.bash_aliases
 
 # System setup
 echo "Update"
@@ -42,17 +49,19 @@ yes | sudo apt-get install tmux
 
 echo "Installing pip and other python packages"
 yes | sudo apt-get install python3-pip 
-yes | pip3 install opencv-python
 yes | pip3 install numpy
 yes | pip3 install matplotlib
 yes | pip3 install pandas
-yes | pip3 install opencv-contrib-python
 yes | pip3 install scipy
-yes | pip3 install casadi
-yes | pip3 install scikit-learn
-yes | pip3 install optuna
-yes | pip3 install optuna-dashboard
-yes | pip3 install optuna-fast-fanova gunicorn
+
+
+# yes | pip3 install opencv-contrib-python
+# yes | pip3 install opencv-python
+# yes | pip3 install casadi
+# yes | pip3 install scikit-learn
+# yes | pip3 install optuna
+# yes | pip3 install optuna-dashboard
+# yes | pip3 install optuna-fast-fanova gunicorn
 
 
 # Install software only if OS is Native Linux
@@ -91,36 +100,81 @@ fi
 
 # ROS Setup
 echo "Installing ROS $ROS_DISTRO"
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-yes | sudo apt install curl # if you haven't already installed curl
-curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
-sudo apt-get update
-sudo apt-get install -y ros-$ROS_DISTRO-desktop-full
-sudo apt-get install -y ros-$ROS_DISTRO-navigation ros-$ROS_DISTRO-teb-local-planner* ros-$ROS_DISTRO-ros-control 
-sudo apt-get install -y ros-$ROS_DISTRO-ros-controllers ros-$ROS_DISTRO-gazebo-ros-control ros-$ROS_DISTRO-ackermann-msgs 
-sudo apt-get install -y ros-$ROS_DISTRO-serial ros-$ROS_DISTRO-rosserial*
-sudo apt-get install -y ros-$ROS_DISTRO-rosbridge-suite ros-$ROS_DISTRO-foxglove-bridge
 
-yes | sudo pip3 install -U catkin_tools
-yes | sudo apt-get install libqt5serialport5-dev
+if [ "$distro" = "focal" ]; then
+  sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+
+  yes | sudo apt install curl # if you haven't already installed curl
+  curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+
+  sudo apt-get update
+
+  sudo apt-get install -y ros-$ROS_DISTRO-desktop-full
+  sudo apt-get install -y ros-$ROS_DISTRO-navigation ros-$ROS_DISTRO-teb-local-planner* ros-$ROS_DISTRO-ros-control 
+  sudo apt-get install -y ros-$ROS_DISTRO-ros-controllers ros-$ROS_DISTRO-gazebo-ros-control ros-$ROS_DISTRO-ackermann-msgs 
+  sudo apt-get install -y ros-$ROS_DISTRO-serial ros-$ROS_DISTRO-rosserial*
+  sudo apt-get install -y ros-$ROS_DISTRO-rosbridge-suite ros-$ROS_DISTRO-foxglove-bridge
+
+  
+  yes | sudo pip3 install -U catkin_tools
+  yes | sudo apt-get install libqt5serialport5-dev
+
+elif [ "$distro" = "jammy" ]; then
+  locale  # check for UTF-8
+
+  sudo apt-get update && sudo apt-get install -y locales
+  sudo locale-gen en_US en_US.UTF-8
+  sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+  export LANG=en_US.UTF-8
+
+  locale  # verify settings
+
+  sudo apt install -y software-properties-common
+  echo | sudo add-apt-repository universe
+
+  sudo apt update && sudo apt install curl -y
+  sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+  
+  sudo apt update
+  sudo apt upgrade
+
+  sudo apt install -y ros-$ROS_DISTRO-desktop
+  sudo apt-get install -y ros-$ROS_DISTRO-rosbridge-suite ros-$ROS_DISTRO-foxglove-bridge
+
+  sudo apt install -y python3-colcon-common-extensions
+
+else
+  echo "Unknown Linux version"
+fi
+
+# yes | sudo apt-get install cmake
 yes | sudo apt-get install python3-rosdep
-
 
 # ROS catkin WS Setup
 source /opt/ros/$ROS_DISTRO/setup.bash
 cd $catkinRoot
 mkdir -p catkin_$ROS_DISTRO/src
 cd catkin_$ROS_DISTRO/
-catkin build;
+
+# init catkin workspace
+if [ "$distro" = "focal" ]; then
+  catkin build;
+elif [ "$distro" = "jammy" ]; then
+  colcon build;
+else
+  echo "Unknown Linux version"
+fi
 
 sudo rosdep init;
 rosdep update;
 
-sudo cp bash_aliases.sh  ~/.bash_aliases
 source ~/.bashrc 
 # source ~/.bash_aliases
 
-
+# cdset_catkin;
+# rset_catkin;
 
 # # Install software only if OS is Native Linux
 # if ! $isWSL; then
--- bash_aliases.sh
+++ bash_aliases.sh
@@ -4,23 +4,30 @@
 # <name> ALL=(ALL) NOPASSWD: ALL
 # %sys ALL=(ALL) NOPASSWD: ALL
 
+
+# sudo cp ./bash_aliases.sh  ~/.bash_aliases
+
 # Detect WSL vs Native Linux
 if grep -qi microsoft /proc/version; then
   # echo "Ubuntu on Windows"
   isWSL=true
-  catkinRoot = "/mnt/c/"
+  catkinRoot="/mnt/c"
 else
   # echo "Native Linux"
   isWSL=false
-  catkinRoot = "~/"
+  catkinRoot="~"
 fi
 
 # Detect Linux version codename
 distro=$(lsb_release -c -s)
 if [ "$distro" = "focal" ]; then
   echo "Running on Ubuntu 20.04 - Focal Fossa"
+  export ROS_DISTRO="noetic"
 elif [ "$distro" = "jammy" ]; then
-  echo "Running on Ubuntu 21.10 - Jammy Jellyfish"
+  echo "Running on Ubuntu 22.04 - Jammy Jellyfish"
+  export ROS_DISTRO="humble"
+  export ROS_DOMAIN_ID=200
+  export ROS_LOCALHOST_ONLY=0
 else
   echo "Unknown Linux version"
 fi
@@ -29,19 +36,39 @@ fi
 source /opt/ros/$ROS_DISTRO/setup.bash
 
 # ROS Aliases
-alias cdset_catkin="cd $catkinRoot/catkin_$ROS_DISTRO/ && clear"
-alias srcset_catkin="source $catkinRoot/catkin_$ROS_DISTRO/devel/setup.bash"
-alias rset_catkin="(
-if cdset_catkin && catkin build; then 
-	source devel/setup.bash &&
-	clear &&
-	echo 'ROS make and source completed.'
-else
-	echo 'ROS make failed.'
-fi
-)"
 
-alias xtf_messages="2> >(grep -v TF_REPEATED_DATA buffer_core)"
+if [ "$distro" = "focal" ]; then
+	alias cdset_catkin="cd $catkinRoot/catkin_$ROS_DISTRO/ && clear"
+	alias srcset_catkin="source $catkinRoot/catkin_$ROS_DISTRO/devel/setup.bash"
+	alias rset_catkin="(
+	if cdset_catkin && catkin build; then 
+		source devel/setup.bash &&
+		clear &&
+		echo 'ROS make and source completed.'
+	else
+		echo 'ROS make failed.'
+	fi
+	)"
+	
+
+	# alias xtf_messages="2> >(grep -v TF_REPEATED_DATA buffer_core)"
+
+elif [ "$distro" = "jammy" ]; then
+	source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash
+  	alias cdset_catkin="cd $catkinRoot/catkin_$ROS_DISTRO/ && clear"
+	alias srcset_catkin="source $catkinRoot/catkin_$ROS_DISTRO/install/setup.bash"
+	alias rset_catkin="(
+	if cdset_catkin && colcon build --symlink-install; then 
+		srcset_catkin &&
+		clear &&
+		echo 'ROS make and source completed.'
+	else
+		echo 'ROS make failed.'
+	fi
+	)"
+else
+  echo "Unknown Linux version"
+fi
 
 srcset_catkin
 
